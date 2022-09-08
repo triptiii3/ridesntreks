86ab1d735c3ca24ef530d11a8f220f4f5edb24b8
triptiii3 <111265499+triptiii3@users.noreply.github.com>
2022-09-08 20:01:41 +0530

    lasts

--- ridesntreks/settings.py
+++ ridesntreks/settings.py
@@ -123,19 +123,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
-MEDIA_URL = '/images/'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,"static_cdn"),
-]
-MEDIA_ROOT = (
-    os.path.join(BASE_DIR,'images')
-)
+
 
