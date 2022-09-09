a57f37d388b0bbcd639f78524242a27660eeb7b9
triptiii3 <111265499+triptiii3@users.noreply.github.com>
2022-09-09 21:38:00 +0530

    latstss

--- home/admin.py
+++ home/admin.py
@@ -1,21 +1,21 @@
-# from django.contrib import admin
-# from home.models import *
+from django.contrib import admin
+from home.models import *
 
-# # Register your models here.
-# admin.site.register(destinations)
-# admin.site.register(Contact)
-# admin.site.register(firsts)
-# admin.site.register(bikingtrip)
-# admin.site.register(roadtrip)
-# admin.site.register(backpackingtrip)
+# Register your models here.
+admin.site.register(destinations)
+admin.site.register(Contact)
 
-# admin.site.register(weekendtrip)
-# admin.site.register(alltrip)
-# admin.site.register(spititrip)
-# admin.site.register(lehtrip)
-# admin.site.register(himachaltrip)
-# admin.site.register(dubaitrip)
-# admin.site.register(bhutantrip)
-# admin.site.register(kashmirtrip)
-# admin.site.register(sikkimtrip)
-# admin.site.register(meghalayatrip)
+admin.site.register(bikingtrip)
+admin.site.register(roadtrip)
+admin.site.register(backpackingtrip)
+
+admin.site.register(weekendtrip)
+admin.site.register(alltrip)
+admin.site.register(spititrip)
+admin.site.register(lehtrip)
+admin.site.register(himachaltrip)
+admin.site.register(dubaitrip)
+admin.site.register(bhutantrip)
+admin.site.register(kashmirtrip)
+admin.site.register(sikkimtrip)
+admin.site.register(meghalayatrip)
