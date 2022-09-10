7ad498852d0ebfcdf410c9a63f0bdcec39002de1
triptiii3 <111265499+triptiii3@users.noreply.github.com>
2022-09-10 11:21:47 +0530

    laststs

--- ridesntreks/settings.py
+++ ridesntreks/settings.py
@@ -131,7 +131,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID='AKIATHEAAY5KWBY7BQGQ'
+AWS_SECRET_ACCESS_KEY='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'
+AWS_STORAGE_BUCKET_NAME='ridesimages'
+AWS_QUERYSTRING_AUTH=False
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
