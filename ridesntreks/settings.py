4164501ed558e525517ba5f986d19cc7ece88257
triptiii3 <111265499+triptiii3@users.noreply.github.com>
2022-10-16 10:18:13 +0530

    last

--- ridesntreks/settings.py
+++ ridesntreks/settings.py
@@ -130,7 +130,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
